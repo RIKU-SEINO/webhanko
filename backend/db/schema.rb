c41d5ab152bdab2b7dfdcf67a0002518817431fe
RIKU-SEINO <rikuseino33@gmail.com>
2025-02-16 18:00:08 +0900

    feat: スキーマを変更

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_13_112945) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_16_070315) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,6 +22,21 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_13_112945) do
     t.index ["jti"], name: "index_jwt_denylists_on_jti", unique: true
   end
 
+  create_table "stamp_downloads", force: :cascade do |t|
+    t.string "stamp_category", default: "", null: false
+    t.string "stamp_type", default: "", null: false
+    t.string "engraving_type", default: "", null: false
+    t.string "font", default: "", null: false
+    t.string "text_1"
+    t.string "text_2"
+    t.string "text_3"
+    t.boolean "is_advanced", default: false, null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_stamp_downloads_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -30,7 +45,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_13_112945) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "stamp_downloads", "users"
 end
