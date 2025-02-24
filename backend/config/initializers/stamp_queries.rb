138c8166344a663c1b8a4c44634643b0db25e595
RIKU-SEINO <rikuseino33@gmail.com>
2025-02-24 13:27:48 +0900

    feat: 外部APIのクエリパラメータとの対応関係を表記

---
+++ backend/config/initializers/stamp_queries.rb
@@ -0,0 +1,378 @@
+Rails.application.config.stamp_queries = {
+
+	font: {
+		personal: {
+			official: {
+				insotai: '0',
+				tenshotai: '1',
+				kointai: '2',
+				reishotai: '3',
+				gyoushotai: '4',
+				kaishotai: '5',
+			},
+			approval: {
+				insotai: '0',
+				tenshotai: '1',
+				kointai: '2',
+				reishotai: '3',
+				gyoushotai: '4',
+				kaishotai: '5',
+			},
+			shachihata: {
+				no_option: '16',
+			},
+		},
+		company: {
+			representative: {
+				insotai: '0',
+				tenshotai: '1',
+				kointai: '2',
+				reishotai: '3',
+				gyoushotai: '4',
+				kaishotai: '5',
+			},
+			square: {
+				insotai: '0',
+				tenshotai: '1',
+				kointai: '2',
+				reishotai: '3',
+				gyoushotai: '4',
+				kaishotai: '5',
+			},
+		},
+		date: {
+			circle: {
+				minchotai: '0',
+				bold_mincho: '1',
+				square_gothic: '2',
+				circle_gothic: '4',
+				kaishotai: '6',
+				gyoushotai: '7',
+				kointai: '8',
+				reishotai: '9',
+				tenshotai: '12',
+				soushotai: '11',
+				insotai: '13',
+			},
+			square: {
+				minchotai: '0',
+				bold_mincho: '1',
+				square_gothic: '2',
+				circle_gothic: '4',
+				kaishotai: '6',
+				gyoushotai: '7',
+				kointai: '8',
+				reishotai: '9',
+				tenshotai: '12',
+				soushotai: '11',
+				insotai: '13',
+			},
+		},
+	},
+
+	command: {
+		personal: {
+			official: {
+				one_row: 'kojinin',
+				one_row_old: 'kojinin',
+				two_rows: 'kojinin',
+				one_col: 'kojinin',
+				two_cols: 'kojinin',
+			},
+			approval: {
+				one_row: 'kojinin',
+				one_row_old: 'kojinin',
+				two_rows: 'kojinin',
+				one_col: 'kojinin',
+				two_cols: 'kojinin',
+			},
+			shachihata: {
+				no_option: 'kojinin',
+			},
+		},
+		company: {
+			representative: {
+				circle_center: 'yakusyokuin',
+				circle_center_one_row: 'yakusyokuin',
+				circle_center_two_rows: 'yakusyokuin',
+				no_circle: 'shikakusyain',
+				no_circle_one_row: 'shikakusyain',
+				no_circle_two_rows: 'shikakusyain',
+			},
+			square: {
+				row: 'kakuin',
+				one_row: 'kakuin',
+				two_rows: 'kakuin',
+				three_rows: 'kakuin',
+				col: 'kakuin',
+				one_col: 'kakuin',
+				two_cols: 'kakuin',
+				three_cols: 'kakuin',
+			},
+		},
+		date: {
+			circle: {
+				no_option: 'dateStamp',
+			},
+			square: {
+				no_option: 'dateStampKaku',
+			},
+		},
+	},
+
+	type: {
+		personal: {
+			official: {
+				one_row: '3',
+				one_row_old: '1',
+				two_rows: '4',
+				one_col: '2',
+				two_cols: '0',
+			},
+			approval: {
+				one_row: '3',
+				one_row_old: '1',
+				two_rows: '4',
+				one_col: '2',
+				two_cols: '0',
+			},
+			shachihata: {
+				no_option: '2',
+			}
+		},
+		company: {
+			representative: {
+				circle_center: '0',
+				circle_center_one_row: '0',
+				circle_center_two_rows: '0',
+				no_circle: '1',
+				no_circle_one_row: '1',
+				no_circle_two_rows: '1',
+			},
+			square: {
+				row: '',
+				one_row: '',
+				two_rows: '',
+				three_rows: '',
+				col: '',
+				one_col: '',
+				two_cols: '',
+				three_cols: '',
+			},
+		},
+		date: {
+			circle: {
+				no_option: '',
+			},
+			square: {
+				no_option: '',
+			},
+		},
+	},
+
+	column_size: {
+		personal: {
+			official: {
+				one_row: '',
+				one_row_old: '',
+				two_rows: '',
+				one_col: '',
+				two_cols: '',
+			},
+			approval: {
+				one_row: '',
+				one_row_old: '',
+				two_rows: '',
+				one_col: '',
+				two_cols: '',
+			},
+			shachihata: {
+				no_option: '',
+			}
+		},
+		company: {
+			representative: {
+				circle_center: '0',
+				circle_center_one_row: '1',
+				circle_center_two_rows: '2',
+				no_circle: '0',
+				no_circle_one_row: '1',
+				no_circle_two_rows: '2',
+			},
+			square: {
+				row: '0',
+				one_row: '1',
+				two_rows: '2',
+				three_rows: '3',
+				col: '0',
+				one_col: '1',
+				two_cols: '2',
+				three_cols: '3',
+			},
+		},
+		date: {
+			circle: {
+				no_option: '',
+			},
+			square: {
+				no_option: '',
+			},
+		},
+	},
+
+	frame_width: {
+		personal: {
+			official: {
+				one_row: '4',
+				one_row_old: '4',
+				two_rows: '4',
+				one_col: '4',
+				two_cols: '4',
+			},
+			approval: {
+				one_row: '4',
+				one_row_old: '4',
+				two_rows: '4',
+				one_col: '4',
+				two_cols: '4',
+			},
+			shachihata: {
+				no_option: '4',
+			}
+		},
+		company: {
+			representative: {
+				circle_center: '6',
+				circle_center_one_row: '6',
+				circle_center_two_rows: '6',
+				no_circle: '6',
+				no_circle_one_row: '6',
+				no_circle_two_rows: '6',
+			},
+			square: {
+				row: '6',
+				one_row: '6',
+				two_rows: '6',
+				three_rows: '6',
+				col: '6',
+				one_col: '6',
+				two_cols: '6',
+				three_cols: '6',
+			},
+		},
+		date: {
+			circle: {
+				no_option: '6',
+			},
+			square: {
+				no_option: '6',
+			},
+		},
+	},
+
+	vector: {
+		personal: {
+			official: {
+				one_row: '',
+				one_row_old: '',
+				two_rows: '',
+				one_col: '',
+				two_cols: '',
+			},
+			approval: {
+				one_row: '',
+				one_row_old: '',
+				two_rows: '',
+				one_col: '',
+				two_cols: '',
+			},
+			shachihata: {
+				no_option: '',
+			}
+		},
+		company: {
+			representative: {
+				circle_center: '',
+				circle_center_one_row: '',
+				circle_center_two_rows: '',
+				no_circle: '',
+				no_circle_one_row: '',
+				no_circle_two_rows: '',
+			},
+			square: {
+				row: '0',
+				one_row: '0',
+				two_rows: '0',
+				three_rows: '0',
+				col: '1',
+				one_col: '1',
+				two_cols: '1',
+				three_cols: '1',
+			},
+		},
+		date: {
+			circle: {
+				no_option: '',
+			},
+			square: {
+				no_option: '',
+			},
+		},
+	},
+
+	balance: {
+		'large': '0',
+		'medium': '1',
+		'small': '2',
+	},
+
+	text_keys: {
+		personal: {
+			official: {
+				one_row: ['stampNamePersonal1', 'dummyQuery', 'dummyQuery'],
+				one_row_old: ['stampNamePersonal1', 'dummyQuery', 'dummyQuery'],
+				two_rows: ['stampNamePersonal1', 'stampNamePersonal2', 'dummyQuery'],
+				one_col: ['stampNamePersonal1', 'dummyQuery', 'dummyQuery'],
+				two_cols: ['stampNamePersonal1', 'stampNamePersonal2', 'dummyQuery'],
+			},
+			approval: {
+				one_row: ['stampNamePersonal1', 'dummyQuery', 'dummyQuery'],
+				one_row_old: ['stampNamePersonal1', 'dummyQuery', 'dummyQuery'],
+				two_rows: ['stampNamePersonal1', 'stampNamePersonal2', 'dummyQuery'],
+				one_col: ['stampNamePersonal1', 'dummyQuery', 'dummyQuery'],
+				two_cols: ['stampNamePersonal1', 'stampNamePersonal2', 'dummyQuery'],
+			},
+			shachihata: {
+				no_option: ['stampNamePersonal1', 'dummyQuery', 'dummyQuery'],
+			}
+		},
+		company: {
+			representative: {
+				circle_center: ['stampNameCorporation', 'stampNameBussiness', 'dummyQuery'],
+				circle_center_one_row: ['stampNameCorporation', 'stampNameBussiness1', 'dummyQuery'],
+				circle_center_two_rows: ['stampNameCorporation', 'stampNameBussiness1', 'stampNameBussiness2'],
+				no_circle: ['stampNameBussiness', 'dummyQuery', 'dummyQuery'],
+				no_circle_one_row: ['stampNameBussiness1', 'dummyQuery', 'dummyQuery'],
+				no_circle_two_rows: ['stampNameBussiness1', 'stampNameBussiness2', 'dummyQuery'],
+			},
+			square: {
+				row: ['stampNameBussiness', 'dummyQuery', 'dummyQuery'],
+				one_row: ['stampNameBussiness1', 'dummyQuery', 'dummyQuery'],
+				two_rows: ['stampNameBussiness1', 'stampNameBussiness2', 'dummyQuery'],
+				three_rows: ['stampNameBussiness1', 'stampNameBussiness2', 'stampNameBussiness3'],
+				col: ['stampNameBussiness', 'dummyQuery', 'dummyQuery'],
+				one_col: ['stampNameBussiness1', 'dummyQuery', 'dummyQuery'],
+				two_cols: ['stampNameBussiness1', 'stampNameBussiness2', 'dummyQuery'],
+				three_cols: ['stampNameBussiness1', 'stampNameBussiness2', 'stampNameBussiness3'],
+			},
+		},
+		date: {
+			circle: {
+				no_option: ['line1String', 'dateSample', 'line4String'],
+			},
+			square: {
+				no_option: ['line1String', 'dateSample', 'line3String'],
+			},
+		},
+	},
+}
