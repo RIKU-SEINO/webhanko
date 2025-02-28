1b6f0d217f5e60f4d132a05607d9900dad4571b5
RIKU-SEINO <rikuseino33@gmail.com>
2025-02-28 18:16:55 +0900

    feat: 前のコミットのRSpecを実装

--- backend/spec/requests/stamps_spec.rb
+++ backend/spec/requests/stamps_spec.rb
@@ -42,4 +42,30 @@ RSpec.describe "Stamps", type: :request do
       end
     end
   end
+
+  describe "GET /api/v1/stamps/translate" do
+    let(:stamp_options) { Rails.application.config.stamp_options }
+    let(:stamp) { FactoryBot.build(:stamp) }
+
+    it 'returns translated stamp options' do
+      get api_v1_stamps_translate_path(stamp.attributes)
+
+      json = JSON.parse(response.body)
+
+      stamp_category = stamp_options.dig(:stamp_category, stamp.stamp_category.to_sym)
+      stamp_type = stamp_options.dig(:stamp_type, stamp.stamp_category.to_sym, stamp.stamp_type.to_sym)
+      engraving_type = stamp_options.dig(:engraving_type, stamp.stamp_category.to_sym, stamp.stamp_type.to_sym, stamp.engraving_type.to_sym)
+      font = stamp_options.dig(:font, stamp.stamp_category.to_sym, stamp.stamp_type.to_sym, stamp.font.to_sym)
+      balance = stamp_options.dig(:balance, stamp.balance.to_sym)
+
+      aggregate_failures do
+        expect(response).to have_http_status(:ok)
+        expect(json['stamp_category']).to eq(stamp_category)
+        expect(json['stamp_type']).to eq(stamp_type)
+        expect(json['engraving_type']).to eq(engraving_type)
+        expect(json['font']).to eq(font)
+        expect(json['balance']).to eq(balance)
+      end
+    end
+  end
 end
