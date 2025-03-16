0cbce3331f2904f16a6ee19409e3ecdaf90b9792
RIKU-SEINO <rikuseino33@gmail.com>
2025-03-17 08:39:11 +0900

    feat: ユーザーの認証状態で動的にHeaderの内容を変える

--- frontend/app/src/components/Header.tsx
+++ frontend/app/src/components/Header.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { AppBar, Toolbar, Typography, Box, Button } from '@mui/material';
+import { AuthContext } from '../App';
+import { useAuth } from '../hooks/useAuth';
+import { FlashMessageContext } from '../App';
 
 const Header: React.FC = () => {
+  const { loading, isSignedIn, currentUser } = useContext(AuthContext);
+  const { handleSignOut, handleFetchCurrentUser } = useAuth();
+  const { setFlashMessageSuccess, setFlashMessageError } = useContext(FlashMessageContext);
+
+  const profileUrl = () => {
+    if (currentUser) {
+      return `/profile/${currentUser.id}`;
+    }
+    return '/';
+  }
+
+  useEffect(() => {
+    handleFetchCurrentUser();
+  }, []);
+
+  const handleSignOutClick = async () => {
+    try {
+      await handleSignOut();
+      setFlashMessageSuccess('ログアウトしました');
+    } catch (error: any) {
+      setFlashMessageError('ログアウトに失敗しました');
+    }
+  }
+
   return (
     <AppBar position="sticky" sx={{ backgroundColor: 'white', color: 'black' }}>
       <Toolbar>
@@ -9,8 +36,20 @@ const Header: React.FC = () => {
           <a href="/" style={{ color: 'black', textDecoration: 'none' }}>Webハンコ！</a>
         </Typography>
         <Box>
-          <Button color="inherit">サインイン</Button>
-          <Button color="inherit">サインアップ</Button>
+          {loading ? (
+            <></>
+          ) :
+          isSignedIn && currentUser ? (
+            <>
+              <Button color="inherit" href={profileUrl()}>マイページ</Button>
+              <Button color="inherit" onClick={handleSignOutClick}>ログアウト</Button>
+            </>
+          ) : (
+            <>
+              <Button color="inherit" href='/signin'>ログイン</Button>
+              <Button color="inherit" href='/signup'>アカウント作成</Button>
+            </>
+          )}
         </Box>
       </Toolbar>
     </AppBar>
