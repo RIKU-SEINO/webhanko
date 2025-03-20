1a3562d86cbc6f90bbb0fb1a985b5809eef0e63f
RIKU-SEINO <rikuseino33@gmail.com>
2025-03-20 19:32:48 +0900

    refactor: before_actionまわりのリファクタリング

--- backend/app/controllers/api/v1/stamps_controller.rb
+++ backend/app/controllers/api/v1/stamps_controller.rb
@@ -1,14 +1,16 @@
 class Api::V1::StampsController < ApplicationController
+  before_action :set_stamp, only: %i[preview metadata]
+  before_action :set_user, only: %i[preview metadata]
   before_action :validate_stamp, only: %i[preview metadata]
   before_action :check_advanced_access, only: %i[preview]
 
   def preview
-    image = build_stamp_image(stamp)
+    image = build_stamp_image
     send_data image.to_blob, type: 'image/png', disposition: 'inline'
   end
 
   def metadata
-    render json: stamp.metadata
+    render json: @stamp.metadata
   end
 
   private
@@ -29,21 +31,24 @@ class Api::V1::StampsController < ApplicationController
     end
   end
 
-  def stamp
+  def set_user
+    @user = current_user
+  end
+
+  def set_stamp
     @stamp ||= Stamp.new
     @stamp.assign_attributes(stamp_params)
-    @stamp
   end
 
   def validate_stamp
-    return render json: { errors: stamp.errors.full_messages }, status: :unprocessable_entity if stamp.invalid?
+    return render json: { errors: @stamp.errors.full_messages }, status: :unprocessable_entity if @stamp.invalid?
   end
 
-  def build_stamp_image(stamp)
-    stamp.advanced? ? stamp.build_image_advanced : stamp.build_image
+  def build_stamp_image
+    @stamp.advanced? ? @stamp.build_image_advanced : @stamp.build_image
   end
 
   def check_advanced_access
-    render json: { errors: ['You are not signed in'] }, status: :unauthorized if stamp.advanced? && !current_user
+    render json: { errors: ['You are not signed in'] }, status: :unauthorized if @stamp.advanced? && @user.nil?
   end
 end
